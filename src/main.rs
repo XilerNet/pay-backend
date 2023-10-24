8f28f95ef45233f694e1caa2c5fcddaa17cd8d99
--global <--global>
2023-10-24 15:24:56 +0200

    :bug: Added cors urls

--- src/main.rs
+++ src/main.rs
@@ -312,7 +312,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "http://localhost:25202".to_string(),
         ]
     } else {
-        vec![env::var("PROD_MAIN_URL").expect("PROD_MAIN_URL not set")]
+        vec![
+            env::var("PROD_MAIN_URL").expect("PROD_MAIN_URL not set"),
+            "https://www.xiler.net".to_string(),
+            "https://xiler.net".to_string(),
+        ]
     };
 
     let routes = Route::new()
